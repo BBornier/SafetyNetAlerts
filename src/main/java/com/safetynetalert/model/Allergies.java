0e174082b8bb24f8cfbebfb94325740cf3ca4743
Benoît B <benoitbornier@yahoo.fr>
2021-11-24 16:48:07 +0100

    Création getters et setters pour la partie Allergies.

--- src/main/java/com/safetynetalert/model/Allergies.java
+++ src/main/java/com/safetynetalert/model/Allergies.java
@@ -1,5 +1,47 @@
 package com.safetynetalert.model;
 
 public class Allergies {
+	
+	int id;
+	
+	String name;
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Allergies() {
+		super();
+	}
+
+	public Allergies(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
 
 }
