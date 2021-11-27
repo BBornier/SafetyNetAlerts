87f238bc8403f3d1baa7ec8090bd41697d496fa2
Benoît B <benoitbornier@yahoo.fr>
2021-11-27 10:46:31 +0100

    Déplacement de l'ensemble du code du main() sous la méthode run().

--- src/main/java/com/safetynetalert/SafetynetalertApplication.java
+++ src/main/java/com/safetynetalert/SafetynetalertApplication.java
@@ -18,31 +18,31 @@ public class SafetynetalertApplication implements CommandLineRunner {
 	public static void main(String[] args) throws JSONException, IOException, ParseException {
 		SpringApplication.run(SafetynetalertApplication.class, args);
 
-		// parser objet Json au level 1.
-		JSONObject jsonObject = JsonReader.readJsonFromUrl(
-				"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/DA+Java+EN/P5+/data.json"); // Fonctionne !
-
-		System.out.println(jsonObject.get("persons"));
-		System.out.println(jsonObject.get("firestations"));
-		System.out.println(jsonObject.get("medicalrecords"));
-
-		// parser objet Json avec une méthode sans JSONParser pour afficher noms et prénoms au level 2 du Json.
-		JSONObject jsonObject2 = JsonReader.parseJsonPersonsFromUrl(); // Fonctionne !
-		JSONObject jsonObject3 = JsonReader.parseJsonFirestationsFromUrl(); // Fonctionne !
-		JSONObject jsonObject4 = JsonReader.parseJsonMedicalRecordsFromUrl();
-		// JSONObject jsonGet = (JSONObject) jsonObject2.get("persons"); // Ecrit avec Raph. à mettre dans la couche controller (?) StackTrace : org.json.JSONArray and org.json.JSONObject are in unnamed module of loader 'app'
-
-		Person person = new Person();
-		person.setId(1); // set les paramètres
-		person.setFirstName(jsonObject2.getString("firstName")); // Ecriture avec Julien. Set un objet Json en paramètre pour un objet Java. StackTrace : JSONObject["firstName"] not found.
-		// person.setFirstName((String) jsonObject2.get("firstName")); // Ecriture avec Raph. Correspondance entre objet java et JsonObject.
-
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Exemple de manipulation de JSON avec SpringBoot");
 
+		// parser objet Json au level 1.
+				JSONObject jsonObject = JsonReader.readJsonFromUrl(
+						"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/DA+Java+EN/P5+/data.json"); // Fonctionne !
+
+				System.out.println(jsonObject.get("persons"));
+				System.out.println(jsonObject.get("firestations"));
+				System.out.println(jsonObject.get("medicalrecords"));
+
+				// parser objet Json avec une méthode sans JSONParser pour afficher noms et prénoms au level 2 du Json.
+				JSONObject jsonObject2 = JsonReader.parseJsonPersonsFromUrl(); // Fonctionne !
+				JSONObject jsonObject3 = JsonReader.parseJsonFirestationsFromUrl(); // Fonctionne !
+				JSONObject jsonObject4 = JsonReader.parseJsonMedicalRecordsFromUrl(); // ne fonctionne qu'au niveau 0 pas au niveau -1.
+				// JSONObject jsonGet = (JSONObject) jsonObject2.get("persons"); // Ecrit avec Raph. à mettre dans la couche controller (?) StackTrace : org.json.JSONArray and org.json.JSONObject are in unnamed module of loader 'app'
+				
+				Person person = new Person();
+				person.setId(1); // set les paramètres
+				//person.setFirstName(jsonObject2.getString("persons")); // Ecriture avec Julien. Set un objet Json en paramètre pour un objet Java. StackTrace : JSONObject["firstName"] not found.
+				System.out.println(person);
+		
 	}
 
 }
