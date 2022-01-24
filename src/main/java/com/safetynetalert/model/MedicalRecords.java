f4cafc25d0533b0473010c92cb8df55782ded6f9
Benoît B <benoitbornier@yahoo.fr>
2022-01-24 17:37:12 +0100

    Suppression de certaines annotations.

--- src/main/java/com/safetynetalert/model/MedicalRecords.java
+++ src/main/java/com/safetynetalert/model/MedicalRecords.java
@@ -1,6 +1,5 @@
 package com.safetynetalert.model;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -9,11 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
-
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
